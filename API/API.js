3e0a55a5fc45f21e51e7ddb4e73a3b2c8b7c5549
Ameer Alaa <134808125+Ameer2006@users.noreply.github.com>
2024-06-20 14:34:34 +0300

    Update API.js
    change links of images

--- API/API.js
+++ API/API.js
@@ -21,7 +21,7 @@ const products =
             "name": "T-shirt",
             "brand": "Adidas",
             "description": "A stylish T-shirt from Adidas featuring the Real Madrid logo. Made with high-quality materials for comfort and durability.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/files/home-pod-weir_535x.jpg?v=1694523452",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F7nqb12anqb19%2F3DBfGMyHw7c2YdBJOqmMGh%2F7523115f2a8bd053f7cfd3e59d2cf13d%2Fnav-menu-320x320-away.jpg&w=1080&q=100",
             "price": 150
         },
         {
@@ -29,7 +29,7 @@ const products =
             "name": "T-shirt",
             "brand": "Adidas",
             "description": "Another sleek T-shirt option from Adidas, showcasing the Real Madrid emblem. Designed for both comfort and style.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/files/third-pod-bellingham_535x.jpg?v=1694523452",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F7nqb12anqb19%2FRI2DKyCzZi2rlHK2jjGcI%2F8af536a2da2666f2e7992636a72aa1f3%2Fnav-menu-320x320-shop_by_player.jpg&w=1080&q=100",
             "price": 250
         },
         {
@@ -37,7 +37,7 @@ const products =
             "name": "T-shirt",
             "brand": "Adidas",
             "description": "An away jersey T-shirt by Adidas, featuring the iconic Real Madrid colors. Perfect for showing support during away games.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/files/away-pod-alaba_535x.jpg?v=1694523452",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F7nqb12anqb19%2F4S0Y3ahJ0pne482FPdFcrn%2Fa623640fb747d9053dd81d657e859b6e%2Fnav-menu-320x320-gk__1_.jpg&w=1080&q=100",
             "price": 200
         },
         {
@@ -45,7 +45,7 @@ const products =
             "name": "T-shirt",
             "brand": "Adidas",
             "description": "Goalkeeper-themed T-shirt from Adidas, designed with the Real Madrid goalkeeper in mind. Stand out on the field with this unique shirt.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/files/gk-pod-misa_535x.jpg?v=1694523452",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F7nqb12anqb19%2F5ynkHmHhwB1NnQLPiYXyLr%2F1bbc3dbd41fac9f77fbeac2175fda492%2Fnav-menu-320x320-kroos.jpg&w=1080&q=100",
             "price": 300
         },
         {
@@ -53,7 +53,7 @@ const products =
             "name": "Backpack",
             "brand": "Adidas",
             "description": "A sturdy backpack by Adidas, featuring the Real Madrid crest. Ideal for carrying essentials to training sessions or matches.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFBG0056-1_533x.jpg?v=1695115368",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFBG0038-01.jpg%3Fv%3D1687961660&w=384&q=100",
             "price": 120
         },
         {
@@ -61,7 +61,7 @@ const products =
             "name": "football",
             "brand": "Adidas",
             "description": "High-quality football designed by Adidas, featuring the Real Madrid club logo. Perfect for matches or training sessions.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFEQ0068-02_533x.jpg?v=1691153224",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFEQ0077-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=384&q=50",
             "price": 50
         },
         {
@@ -69,7 +69,7 @@ const products =
             "name": "Gloves",
             "brand": "Adidas",
             "description": "Adidas gloves designed for football, featuring the Real Madrid branding. Keep your hands warm and protected during games.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/files/RMCFMK0067_500x480.jpg?v=1686814230",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFEQ0060.jpg%3Fv%3D1657870684&w=384&q=50",
             "price": 70
         },
         {
@@ -77,7 +77,7 @@ const products =
             "name": "scarf",
             "brand": "Adidas",
             "description": "A stylish scarf by Adidas, adorned with the Real Madrid crest. Keep warm while showing your support for the team.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFMK0068-03_720x.jpg?v=1685973681",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMK0073-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=384&q=50",
             "price": 50
         },
         {
@@ -85,7 +85,7 @@ const products =
             "name": "Cap",
             "brand": "Adidas",
             "description": "Adidas cap featuring the Real Madrid logo. Protect yourself from the sun while representing your favorite football club.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFMH0073-01_500x480.jpg?v=1685973669",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FRMCFMH0088-01.jpg%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522applicationId%2522%3A%252201H4RD9NXMKQBQ1WVKM1181VD8%2522%2C%2522tenantId%2522%3A%2522REAL_MADRID%2522%257D&w=384&q=50",
             "price": 90
         },
         {
@@ -93,15 +93,15 @@ const products =
             "name": "Duffle Bag",
             "brand": "Adidas",
             "description": "Spacious duffle bag by Adidas, perfect for carrying gear to and from training sessions or games. Features the Real Madrid emblem.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/files/RMCFBG0019-01_500x480.jpg?v=1687798963",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFBG0022-01.jpg%3Fv%3D1682348909&w=384&q=50",
             "price": 100
         },
         {
             "id": 11,
-            "name": "Trainer Pencil Case",
+            "name": "Pencil Case",
             "brand": "Adidas",
             "description": "Pencil case designed like a trainer, perfect for storing school or office supplies. Features Real Madrid branding.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFOF0017-01_500x480.jpg?v=1688634083",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFOF0006-01.jpg%3Fv%3D1682348932&w=384&q=50",
             "price": 80
         },
         {
@@ -109,7 +109,7 @@ const products =
             "name": "Black Ceramic Mug crest",
             "brand": "Adidas",
             "description": "Black ceramic mug featuring the Real Madrid crest. Enjoy your favorite beverage while showing support for the team.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFBV0034-02_500x480.jpg?v=1664971435",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFBV0053-01.jpg%3Fv%3D1706696615&w=384&q=50",
             "price": 75
         },
         {
@@ -117,7 +117,7 @@ const products =
             "name": "Crest Keyring",
             "brand": "Adidas",
             "description": "Keyring featuring the Real Madrid crest. Perfect for keeping your keys organized while displaying your team pride.",
-            "imgUrl": " https://shop.realmadrid.com/cdn/shop/products/Real-Madrid-Crest-Keyring_500x480.jpg?v=1598480512",
+            "imgUrl": " https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FReal-Madrid-Crest-Keyring.jpg%3Fv%3D1592821394&w=384&q=50",
             "price": 60
         },
         {
@@ -125,7 +125,7 @@ const products =
             "name": "Crest Reversible Beanie ",
             "brand": "Adidas",
             "description": "Reversible beanie featuring the Real Madrid crest. Keep warm in style during the colder months.",
-            "imgUrl": "https://shop.realmadrid.com/cdn/shop/products/RMCFYK0009_01_533x.jpg?v=1642255200",
+            "imgUrl": "https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFYK0009_01_11b333c3-58f3-451e-9faf-e239bb2be2ed.jpg%3Fv%3D1642255540&w=384&q=50",
             "price": 30
         },
         {
@@ -133,7 +133,7 @@ const products =
             "name": "Bracelet  ",
             "brand": "Adidas",
             "description": "Bracelet featuring the Real Madrid logo. A subtle accessory to show your support for the team.",
-            "imgUrl": " https://shop.realmadrid.com/cdn/shop/products/RMCFNV0387-01_500x480.jpg?v=1691486309",
+            "imgUrl": " https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Fproducts%2FRMCFNV0787-02.jpg%3Fv%3D1679581867&w=384&q=50",
             "price": 10
         },
         {
@@ -141,7 +141,7 @@ const products =
             "name": "Viceroy Watch",
             "brand": "Adidas",
             "description": "Stylish watch by Viceroy featuring the Real Madrid logo. Keep track of time while showcasing your support for your team",
-            "imgUrl": " https://shop.realmadrid.com/cdn/shop/products/RMCFJB0003-01_500x480.jpg?v=1682348873",
+            "imgUrl": " https://shop.realmadrid.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0405%2F9807%2F7603%2Ffiles%2FRMCFJB0008-01.jpg%3Fv%3D1689165560&w=384&q=50",
             "price": 120
         }
     ]
